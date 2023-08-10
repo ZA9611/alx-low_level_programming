d7697a23c233fead9c55b3b2baa2abf1cd403630
zakaria <zakariaassaka3@gmail.com>
2023-08-10 11:08:47 -0700

    zkaria

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -3,8 +3,9 @@
 
 /**
  * *malloc_checked - allocates memory using malloc
- * @b: number of bytes to allocate 
- * return: a pointer to the allocated memory
+ * @b: number of bytes to allocate
+ *
+ * Return: a pointer to the allocated memory
  */
 void *malloc_checked(unsigned int b)
 {
